db17359a22c648be2027f1e2e30fb47d025366be
slow-charles <115585837+slow-charles@users.noreply.github.com>
2023-01-24 15:14:08 -0300

    Add files via upload

---
+++ Remix_ PRO 1_4 C9 AP2/Remix: PRO 1:4 C9 AP2/code.js
@@ -0,0 +1,93 @@
+var p5Inst = new p5(null, 'sketch');
+
+window.preload = function () {
+  initMobileControls(p5Inst);
+
+  p5Inst._predefinedSpriteAnimations = {};
+  p5Inst._pauseSpriteAnimationsByDefault = false;
+  var animationListJSON = {"orderedKeys":[],"propsByKey":{}};
+  var orderedKeys = animationListJSON.orderedKeys;
+  var allAnimationsSingleFrame = false;
+  orderedKeys.forEach(function (key) {
+    var props = animationListJSON.propsByKey[key];
+    var frameCount = allAnimationsSingleFrame ? 1 : props.frameCount;
+    var image = loadImage(props.rootRelativePath, function () {
+      var spriteSheet = loadSpriteSheet(
+          image,
+          props.frameSize.x,
+          props.frameSize.y,
+          frameCount
+      );
+      p5Inst._predefinedSpriteAnimations[props.name] = loadAnimation(spriteSheet);
+      p5Inst._predefinedSpriteAnimations[props.name].looping = props.looping;
+      p5Inst._predefinedSpriteAnimations[props.name].frameDelay = props.frameDelay;
+    });
+  });
+
+  function wrappedExportedCode(stage) {
+    if (stage === 'preload') {
+      if (setup !== window.setup) {
+        window.setup = setup;
+      } else {
+        return;
+      }
+    }
+// -----
+
+var life = 0;
+var car1, car2, car3,car4;
+var boundary1, boundary2;
+var sam;
+
+  boundary1 = createSprite(190,120,420,3);
+  boundary2 = createSprite(190,260,420,3);
+  
+  sam = createSprite(20,190,13,13);
+  sam.shapeColor = "green";
+  
+  car1 = createSprite(100,130,10,10);
+  car1.shapeColor = "red";
+  car2 = createSprite(215,130,10,10);
+  car2.shapeColor = "red";
+  car3 = createSprite(165,250,10,10);
+  car3.shapeColor = "red";
+  car4 = createSprite(270,250,10,10);
+  car4.shapeColor = "red";
+  
+ 
+//adicione velocidade para fazer o carro se mover.
+
+function draw() {
+   background("white");
+  text("Vidas: " + life,200,100);
+  strokeWeight(0);
+  fill("lightblue");
+  rect(0,120,52,140);
+  fill("yellow");
+  rect(345,120,52,140);
+  
+// crie a função rebater, para fazer o carro rebater nos limites
+//Adicione a condição para fazer Sam se mover para a esquerda e para a direita
+//Adicione a condição para reduzir a vida de Sam quando ele encostar no carro.
+  
+ drawSprites();
+}
+
+// -----
+    try { window.draw = draw; } catch (e) {}
+    switch (stage) {
+      case 'preload':
+        if (preload !== window.preload) { preload(); }
+        break;
+      case 'setup':
+        if (setup !== window.setup) { setup(); }
+        break;
+    }
+  }
+  window.wrappedExportedCode = wrappedExportedCode;
+  wrappedExportedCode('preload');
+};
+
+window.setup = function () {
+  window.wrappedExportedCode('setup');
+};
